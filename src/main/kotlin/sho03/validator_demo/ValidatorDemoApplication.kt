b49c230cc3eefc63736ea33f99ab87badf74b8d7
sho ochiai <ochi1020@gmail.com>
2024-10-27 13:08:02 +0900

    feat: create project.

---
+++ src/main/kotlin/sho03/validator_demo/ValidatorDemoApplication.kt
@@ -0,0 +1,11 @@
+package sho03.validator_demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ValidatorDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<ValidatorDemoApplication>(*args)
+}
