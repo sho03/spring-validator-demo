a4203cbcd2062c16a96f11934359153f50cc6e14
sho ochiai <ochi1020@gmail.com>
2024-10-27 13:50:37 +0900

    feat: create GlobalExceptionHandler to handle method argument error.

---
+++ src/main/kotlin/sho03/validator_demo/controller/GlobalExceptionHandler.kt
@@ -0,0 +1,25 @@
+package sho03.validator_demo.controller
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.FieldError
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleValidationExceptions(e: MethodArgumentNotValidException): ResponseEntity<Map<String, String>> {
+        val errors = mutableMapOf<String, String>()
+
+        e.bindingResult.allErrors.forEach { error ->
+            val fieldName = (error as FieldError).field
+            val errorMessage = error.defaultMessage ?: "Invalid value"
+            errors[fieldName] = errorMessage
+        }
+
+        return ResponseEntity(errors, HttpStatus.BAD_REQUEST)
+    }
+}
